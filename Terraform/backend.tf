089ed9423a4095705fe76b254f3f77d3f5e2fda5
Carneiro1908 <tomasclima1406@gmail.com>
2026-06-29 10:27:11 +0100

    Fix: duplicate providers configuration

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,4 @@
 terraform {
-  required_version = ">= 1.5.0"
-  
   # Commercial Remote Backend Configuration
   backend "s3" {
     bucket         = "comercial-k8s-protected-storage-2026"
@@ -8,12 +6,5 @@ terraform {
     region         = "eu-north-1"
     encrypt        = true
     dynamodb_table = "comercial-k8s-terraform-locks"
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
   }
 }
